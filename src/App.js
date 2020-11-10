282a3347693f099b00263b85d66534393642dce2
Bo De Corte <67636455+BoDC@users.noreply.github.com>
2020-11-10 13:32:25 +0100

    added URL

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 
 export default function App() {
   return <div className="App">
-   Weather App
-    </div>;
+   <h1>Weather App</h1>
+   <footer>
+     This project was coded by Bo De Corte and is
+   <a href="https://github.com/BoDC/weather-react" target="_blank"> open-sourced on Github</a>
+    
+    </footer>
+     </div>;
 }
 
 
